db89be0d9b2e968a3e24307e6905d3128fde5c15
Ben Lister <admin@twentyfourframes.com.au>
2019-08-17 13:57:34 +1000

    update table styling

--- includes/elements/resources.php
+++ includes/elements/resources.php
@@ -10,21 +10,21 @@
   </thead>
   <tbody>
     <tr>
-      <td>Sky tower exterior</td>
+      <td><i class="fas fa-cube"></i>Sky tower exterior</td>
       <td>[filetype]</td>
       <td>Me</td>
       <td>18/07/2019 11:06:21</td>
       <td>8.3Mb</td>
     </tr>
     <tr>
-      <td>Sky tower lobby</td>
+      <td><i class="fas fa-cube"></i>Sky tower lobby</td>
       <td>[filetype]</td>
       <td>Kyle Samuels</td>
       <td>21/07/2019 16:42:21</td>
       <td>12.1Mb</td>
     </tr>
     <tr>
-      <td>Sky tower auditorium</td>
+      <td><i class="fas fa-cube"></i>Sky tower auditorium</td>
       <td>[filetype]</td>
       <td>Ty Church</td>
       <td>06/08/2019 13:12:52</td>
